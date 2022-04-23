e55e94329f73dfdaebffebdb300b1f4b3da9bd23
Gabriel_Coder <82976811+gabtapia@users.noreply.github.com>
2022-04-23 12:40:50 -0300

    Canos Desenhados e Movimentados

--- jogo.js
+++ jogo.js
@@ -177,23 +177,93 @@ function mudaParaTela(novaTela) {
     }
 }
 
+function criaCanos() {
+    const canos = {
+        largura: 52, 
+        altura: 400,
+        chao: {
+            spriteX: 0, 
+            spriteY: 169,     
+        }, 
+        ceu: {
+            spriteX: 52, 
+            spriteY: 169,     
+        }, 
+        espaco: 80, 
+        desenha() {
+            canos.pares.forEach(function(par) {
+                const yRandom = par.y;
+                const espacamentoEntreCanos = 40;
+    
+                const canoCeuX = par.x;
+                const canoCeuY = yRandom;
+    
+                // [Cano Do Céu]
+                ctx.drawImage(
+                    sprites, 
+                    canos.ceu.spriteX, canos.ceu.spriteY, 
+                    canos.largura, canos.altura, 
+                    canoCeuX, canoCeuY, 
+                    canos.largura, canos.altura
+                );
+    
+                const canoChaoX = par.x;
+                const canoChaoY = canos.altura + espacamentoEntreCanos + yRandom;
+                
+                // [Cano do Chão]
+                ctx.drawImage(
+                    sprites, 
+                    canos.chao.spriteX, canos.chao.spriteY, 
+                    canos.largura, canos.altura, 
+                    canoChaoX, canoChaoY, 
+                    canos.largura, canos.altura
+                );     
+            })
+
+        }, 
+        pares: [], 
+        atualiza() {
+            const passou100Frames = frames % 100 === 0;
+            if(passou100Frames) {
+                canos.pares.push({
+                    x: canvas.width, 
+                    y: -150 * (Math.random() + 1)
+                });
+            }
+
+            canos.pares.forEach(function(par) {
+                par.x = par.x - 2;
+
+                if(par.x + canos.largura <= 0) {
+                    canos.pares.shift();
+                }
+            })
+        }
+    }
+
+    return canos;
+}
+
 const Telas = {
     INICIO: {
         inicializa() {
             globais.flappyBird = criaFlappyBird();
             globais.chao = criaChao();
+            globais.canos = criaCanos();
         }, 
         desenha() {
             bck.desenha();
+            globais.flappyBird.desenha();
+            globais.canos.desenha();
             globais.chao.desenha();
-            globais.flappyBird.desenha(); 
-            msgGetReady.desenha();
+            // msgGetReady.desenha();
         }, 
         click() {
             mudaParaTela(Telas.JOGO);
         }, 
         atualiza() {
             globais.chao.atualiza();
+            globais.canos.atualiza();
         }
     }
 };
