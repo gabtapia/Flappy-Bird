7d10f75cbd114d609188c991e9e98bc61c24ba19
Gabriel_Coder <82976811+gabtapia@users.noreply.github.com>
2022-04-25 20:51:26 -0300

    Refaturando o Objeto do Flappy Bird

--- jogo.js
+++ jogo.js
@@ -102,21 +102,21 @@ function fazColisao(flappyBird, chao) {
 }
 
 /* [Objetos] */
-function criaFlappyBird() {
-    const flappyBird = {
-        spriteX: 0, 
-        spriteY: 0, 
-        largura: 33, 
-        altura: 24, 
-        x: 10, 
-        y: 50, 
-        pulo: 4.6, 
-        pula() {
-            flappyBird.velocidade = - flappyBird.pulo;
-        }, 
-        gravidade: 0.25, 
-        velocidade: 0, 
-        atualiza() {
+class FlappyBird {
+    constructor() {
+        this.spriteX = 0;
+        this.spriteY = 0;
+        this.largura = 33; 
+        this.altura = 24;
+        this.x = 10;
+        this.y = 50; 
+        this.pulo = 4.6; 
+        function pula() {
+            this.velocidade = - this.pulo;
+        }
+        this.gravidade = 0.25;
+        this.velocidade = 0;
+        function atualiza() {
             if(fazColisao(flappyBird, globais.chao)) {
                 som_HIT.play();
 
@@ -124,42 +124,41 @@ function criaFlappyBird() {
                 return;
             }
     
-            flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
-            flappyBird.y =  flappyBird.y + flappyBird.velocidade;
-        }, 
-        movimentos: [
+            this.velocidade = this.velocidade + this.gravidade;
+            this.y =  this.y + this.velocidade;
+        }
+        movimentos = [
             { spriteX: 0, spriteY: 0 }, 
             { spriteX: 0, spriteY: 26 }, 
             { spriteX: 0, spriteY: 52 }, 
             { spriteX: 0, spriteY: 26 }, 
-        ], 
-        frameAtual: 0, 
-        atualizaOFrameAtual() {
+        ];
+        frameAtual = 0; 
+        function atualizaOFrameAtual() {
             const intervaloDeFrames = 10;
             const passouOIntervalo = frames % intervaloDeFrames === 0;
 
             if(passouOIntervalo) {
                 const baseDoIncremento = 1;
-                const incremento = baseDoIncremento + flappyBird.frameAtual;
-                const baseRepeticao = flappyBird.movimentos.length;
-                flappyBird.frameAtual = incremento % baseRepeticao;
+                const incremento = baseDoIncremento + this.frameAtual;
+                const baseRepeticao = this.movimentos.length;
+                this.frameAtual = incremento % baseRepeticao;
             }
 
-        }, 
-        desenha() {
-            flappyBird.atualizaOFrameAtual();
-            const { spriteX, spriteY } = flappyBird.movimentos[flappyBird.frameAtual];
+        }
+        function desenha() {
+            this.atualizaOFrameAtual();
+            const { spriteX, spriteY } = this.movimentos[this.frameAtual];
 
             ctx.drawImage(
                 sprites, 
                 spriteX, spriteY, 
-                flappyBird.largura, flappyBird.altura, 
-                flappyBird.x, flappyBird.y, 
-                flappyBird.largura, flappyBird.altura
+                this.largura, this.altura, 
+                this.x, this.y, 
+                this.largura, this.altura
             );
         }
     }
-    return flappyBird;
 }
 
 function criaCanos() {
